92cadb68b12ced26ca016256522e6a25e1127342
1504zxnk <131455259+1504zxnk@users.noreply.github.com>
2023-04-28 21:32:27 +0800

    Create ex2-4.cpp

---
+++ ex2-4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+    int a,b;    
+    float h = 0.5;   
+    printf("A=> ");  
+    scanf("%d",&a);   
+    printf("B=> "); 
+    scanf("%d",&b); 
+    printf("A+B=%d\n",a+b); 
+    printf("A-B=%d\n",a-b);  
+    printf("A*B=%d\n",a*b);
+    printf("A/B=%d\n",a/b);   
+    return 0;}
